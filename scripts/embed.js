2b57244a3a244c1968dda210307b0ecaf5ae6f52
iksi <jur@iksi.cc>
2014-04-10 00:34:37 +0200

    Update module pattern

---
+++ scripts/embed.js
@@ -0,0 +1,63 @@
+/**
+ * Embed module
+ */
+var embed = (function () {
+    'use strict';
+    return function (element) {
+
+        var request = function (url, callback) {
+            var req = new XMLHttpRequest();
+            req.open('GET', url, true);
+            req.onload = function () {
+                if (req.status >= 200 && req.status < 400 && callback) {
+                    callback(JSON.parse(req.responseText));
+                }
+            };
+            req.send();
+        };
+
+        var embed = function (data) {
+            if (data.html) {
+                element.innerHTML = data.html;
+                var iframe = element.querySelector('iframe');
+                if (iframe.getAttribute('width') === '100%') {
+                    element.style.paddingTop = iframe.height + 'px'; // Audio
+                } else {
+                    element.style.paddingTop = (100 * iframe.height / iframe.width) + '%'; // Videos
+                }
+            }
+        };
+
+        return {
+            init: function () {
+                var link = element.querySelector('a');
+                if (link) {
+                    request('/alpha.iksi.cc/oembed?url=' + link.href, embed);
+                    element.removeChild(link);
+                }
+            }
+        };
+
+    };
+}());
+
+var embeds = (function () {
+    'use strict';
+
+    return function (selector) {
+
+        return {
+            init: function () {
+                var elements = document.querySelectorAll(selector);
+                [].forEach.call(elements, function (element) {
+                    embed(element).init();
+                });
+            }
+        };
+    };
+}());
+
+(function () {
+    'use strict';
+    embeds('.embed').init();
+}());
