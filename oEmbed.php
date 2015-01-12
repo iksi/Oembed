fa3c08adc0c8765a7e9b7b156df4a747ea8fb95c
Iksi <jur@iksi.cc>
2015-01-12 19:40:25 +0100

    remove response filter
    removed response filter

--- oEmbed.php
+++ oEmbed.php
@@ -12,65 +12,33 @@ namespace Iksi;
 
 class oEmbed
 {
-    protected $config;
-
-    public function __construct()
+    public function get($url)
     {
-        $this->config = require_once(__DIR__ . DIRECTORY_SEPARATOR .'config.php');
-    }
+        $apiUrl = $this->getApiUrl($url);
 
-    public function request($url, $autoplay = null)
-    {
-        $api_url = $this->buildUrl($url);
-
-        if ($api_url === false) {
+        if ($apiUrl === false) {
             return $this->error('something seems wrong with the url');
         }
 
-        $curl_handle = curl_init();
+        $curlHandle = curl_init();
 
-        curl_setopt($curl_handle, CURLOPT_URL, $api_url);
-        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl_handle, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-        curl_setopt($curl_handle, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($curlHandle, CURLOPT_URL, $apiUrl);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curlHandle, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+        curl_setopt($curlHandle, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 
-        $response = curl_exec($curl_handle);
+        $response = curl_exec($curlHandle);
 
-        if (curl_errno($curl_handle)) {
-            return $this->error(curl_error($curl_handle));
+        if (curl_errno($curlHandle)) {
+            return $this->error(curl_error($curlHandle));
         }
 
-        curl_close($curl_handle);
+        curl_close($curlHandle);
 
-        return $this->filterResponse($response, $autoplay);
+        return $response;
     }
 
-    protected function filterResponse($response, $autoplay)
-    {
-        $data = json_decode($response, true);
-
-        $provider = strtolower($data['provider_name']);
-
-        $parameters = array_key_exists($provider, $this->config)
-            ? $this->config[$provider]
-            : array();
-
-        if ($autoplay !== null && $autoplay_key = current(preg_grep('/^auto_?play$/i', array_keys($parameters)))) {
-            $parameters[$autoplay_key] = $autoplay ? 'true' : 'false';
-        }
-
-        preg_match('/src="(?<url>[^"]+)"/', $data['html'], $matches);
-
-        $url = preg_replace('/https?:\/\//', '//', $matches['url'])
-            . (parse_url($matches['url'], PHP_URL_QUERY) ? '&' : '?')
-            . http_build_query($parameters);
-
-        $data['html'] = preg_replace('/src="([^"]+)"/', 'src="' . $url . '"', $data['html']);
-
-        return json_encode($data);
-    }
-
-    protected function buildUrl($url)
+    protected function getApiUrl($url)
     {
         if ( ! filter_var($url, FILTER_VALIDATE_URL)) {
             return false;
@@ -103,6 +71,9 @@ class oEmbed
 
     protected function error($error)
     {
-        return json_encode(array('error' => $error));
+        $response = array(
+            'error' => $error
+        );
+        return json_encode($response);
     }
 }
