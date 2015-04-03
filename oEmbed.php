8614ff413a3507d33b2e6655127636fe91fcb560
iksi <jur@iksi.cc>
2015-04-03 12:20:26 +0200

    Update oEmbed.php

--- oEmbed.php
+++ oEmbed.php
@@ -12,17 +12,19 @@ namespace Iksi;
 
 class oEmbed
 {
-    public function fetch($url)
+    public function get($url)
     {
-        $providerUrl = $this->getProviderUrl($url);
+        $endPoint = $this->endPoint($url);
 
-        if ($providerUrl === false) {
-            return $this->error('something seems wrong with the url');
+        if ($endPoint === false) {
+            return $this->error('no valid endpoint found');
         }
 
         $curlHandle = curl_init();
+        
+        $curlUrl = $endPoint . '?url=' . urlencode($url);
 
-        curl_setopt($curlHandle, CURLOPT_URL, $providerUrl);
+        curl_setopt($curlHandle, CURLOPT_URL, $curlUrl);
         curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curlHandle, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         curl_setopt($curlHandle, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
@@ -37,6 +39,11 @@ class oEmbed
 
         return $response;
     }
+    
+    protected function error($error)
+    {
+        return json_encode(compact('error'));
+    }
 
     protected function getProviderUrl($url)
     {
@@ -47,30 +54,25 @@ class oEmbed
         $host = parse_url($url, PHP_URL_HOST);
         
         if (preg_match('/mixcloud\.com$/', $host)) {
-            return 'https://www.mixcloud.com/oembed/?url=' . $url;
+            return 'https://www.mixcloud.com/oembed/';
         }
         
         if (preg_match('/(soundcloud\.com|snd\.sc)$/', $host)) {
-            return 'https://soundcloud.com/oembed.json?url=' . $url;
+            return 'https://soundcloud.com/oembed.json';
         }
 
         if (preg_match('/spotify\.com$/', $host)) {
-            return 'https://embed.spotify.com/oembed/?url=' . $url;
+            return 'https://embed.spotify.com/oembed/';
         }
 
         if (preg_match('/vimeo\.com$/', $host)) {
-            return 'https://vimeo.com/api/oembed.json?url=' . $url;
+            return 'https://vimeo.com/api/oembed.json';
         }
 
         if (preg_match('/(youtube\.com|youtu\.be)$/', $host)) {
-            return 'https://www.youtube.com/oembed?url=' . $url;
+            return 'https://www.youtube.com/oembed/';
         }
 
         return false;
-    }
-
-    protected function error($error)
-    {
-        return json_encode(compact('error'));
     }
 }
